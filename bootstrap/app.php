143a102840516e5790430bd1f10c781aa8895a3b
syukron <syukron488@gmail.com>
2026-01-10 17:34:28 +0700

    allow iframe in other website

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
 
             'superadmin' => \App\Http\Middleware\superadmin::class,
+            'iframe' => \App\Http\Middleware\AllowIframe::class,
         ]);
 
     })
